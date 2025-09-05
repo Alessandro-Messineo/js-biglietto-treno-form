4fd9e552a15c6f2ea54c2b1d0b2b2612513580cf
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-05 17:51:29 +0200

    milestone 3: aggiunto il css

--- js/script.js
+++ js/script.js
@@ -7,11 +7,18 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
+// MILESTONE 1:
+// Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), 
+// realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console. 
+
 // MILESTONE 2:
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati 
 // e visualizzare il calcolo finale con il prezzo. 
-// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, 
-// per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina 
+// (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// MILESTONE 3:
+// Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
 
 // inserire gli input nelle variabili
 const inputName = document.getElementById("name");
@@ -41,6 +48,6 @@ inputForm.addEventListener("submit", (event) => {
     }
 
     outputPar.innerHTML = `Nome passeggero: ${inputName.value} <br>`+ `Distanza da percorrere: ${km}Km <br>`+`Prezzo del biglietto: € ${price.toFixed(2)}`;
-
+    inputForm.reset();
 })
 
